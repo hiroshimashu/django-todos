b15b9f5c073145564290ad74c905dbd4dec93555
hiroshimashu <hiroshimashu@gmail.com>
2019-03-31 22:18:20 +0900

    finish implement backend

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -40,9 +40,21 @@ INSTALLED_APPS = [
 
     # Local
     'todos.apps.TodosConfig',
+    'corsheaders',
+
+    #3rd party
+    'rest_framework'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +62,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'localhost:3000'
 ]
 
 ROOT_URLCONF = 'todo_project.urls'
